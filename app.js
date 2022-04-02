bb88b5aab5ea6825c542cd3a696c43b7a7f9ddb3
Stacey P <staceypopenfoose@gmail.com>
2022-04-02 00:05:49 -0400

    add active links

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 /* menu movement */
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
+const linkedInLink = document.querySelector('#linkedin')
+const gitHubLink = document.querySelector('#github')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
     menuLinks.classList.toggle('active')
+})
+
+linkedInLink.addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/staceypopenfoose/', '_blank');
+})
+
+gitHubLink.addEventListener('click', function(){
+    window.open('https://github.com/slpop', '_blank');
 })

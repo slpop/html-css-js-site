2363f1430728998742dff26857847893b7530d56
Stacey P <staceypopenfoose@gmail.com>
2022-04-04 21:34:07 -0400

    linkedin and github links

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 const linkedInLink = document.querySelector('#linkedin')
-const gitHubLink = document.querySelector('#github')
+const gitHubLink = document.querySelector('#github-square')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')

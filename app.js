df333e2569b17a2a69ee6077001750288aed2e8a
Stacey P <staceypopenfoose@gmail.com>
2022-04-06 13:58:33 -0400

    start projects.html

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const menu = document.querySelector('#mobile-menu')
 const menuLinks = document.querySelector('.navbar__menu')
 const linkedInLink = document.querySelector('#linkedin')
 const gitHubLink = document.querySelector('#github-square')
+const portfolioLink = document.querySelector('#portfolioLink')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active')
@@ -13,6 +14,10 @@ linkedInLink.addEventListener('click', function() {
     window.open('https://www.linkedin.com/in/staceypopenfoose/', '_blank');
 })
 
-gitHubLink.addEventListener('click', function(){
+gitHubLink.addEventListener('click', function() {
     window.open('https://github.com/slpop', '_blank');
+})
+
+portfolioLink.addEventListener('click', function() {
+    window.open('https://github.com/slpop/portfolio-website', '_blank');
 })
